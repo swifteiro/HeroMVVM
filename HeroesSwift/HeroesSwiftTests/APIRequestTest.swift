72e7bfce801d7f1920a134b1ae741b45e21527d4
Vinicius A. Minozzi <vinicius.minozzi@tritone.com.br>
2016-08-16 18:04:36 -0300

    fixed structTest

--- HeroesSwift/HeroesSwiftTests/APIRequestTest.swift
+++ HeroesSwift/HeroesSwiftTests/APIRequestTest.swift
@@ -11,28 +11,6 @@ import XCTest
 
 class APIRequestTest: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testAPIRequest() {
         XCTAssertEqual(APIRequest.serverAPI, "http://gateway.marvel.com:80")
         XCTAssertEqual(APIRequest.pathAPI, "/v1/public/characters/1009351/comics?")
